0a81b59b17e4612a7b5c55f9b79320e86ba188eb
mrugendrakul <mrugendraskulkarni@gmail.com>
2024-11-01 21:34:16 +0530

    Lates changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 const admin = require('firebase-admin');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { configDotenv } = require('dotenv');
 // const sendNotificationsToDevices = require('./sendNoti')
 // const {sendNotificationsToDevices} = require('./sendNoti')
 const app = express()
@@ -8,12 +10,24 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 const serviceAccount = require('./lockntalk-firebase-adminsdk-bsz1u-098b5e841e.json')
+
 // const { getFirestore, connectFirestoreEmulator } =require( "firebase/firestore");
 // const sendNotificationToDevice = require('./sendNotification.js');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+try{admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    })
 });
+console.log("Initialized");
+
+}
+catch(e){
+    console.error("error initializing : ",e)
+}
 
 app.post('/sendnotfi', (req, res) => {
     console.log("Successfully get request");
@@ -46,16 +60,16 @@ app.post('/sendnotfi', (req, res) => {
         .catch((error) => {
             console.error('Error sending notification:', error);
             res.status(500).json(
-                {message:"Unable to send notifictaion:"+error}
+                { message: "Unable to send notifictaion:" + error }
             )
             // throw error
         });
-   
+
 
 
 })
 
 app.listen(7000, () => {
     console.log("Server started on port : 7000");
-
+    console.log(`Project id : ${serviceAccount.project_id}`)
 })  
